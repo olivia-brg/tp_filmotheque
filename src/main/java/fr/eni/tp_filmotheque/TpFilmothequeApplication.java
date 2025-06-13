7033a57737cd8f9715d78ef573ce694a3216847a
Olivia <olivia.bergaglia@gmail.com>
2025-06-13 16:50:58 +0200

    Thymeleaf preparation

--- src/main/java/fr/eni/tp_filmotheque/TpFilmothequeApplication.java
+++ src/main/java/fr/eni/tp_filmotheque/TpFilmothequeApplication.java
@@ -1,29 +1,13 @@
 package fr.eni.tp_filmotheque;
 
-import fr.eni.tp_filmotheque.ihm.FilmController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TpFilmothequeApplication {
 
     public static void main(String[] args) {
-
-        ApplicationContext context = SpringApplication.run(TpFilmothequeApplication.class, args);
-
-        FilmController controller = context.getBean(FilmController.class);
-
-        // Affichage du film d'identiant 1
-        System.out.println("\nLe film d'identiant 1 est : ");
-        controller.afficherUnFilm(1);
-
-
-        // Affichage de la liste des films
-        System.out.println("\n");
-        controller.afficherFilms();
-
-
+        SpringApplication.run(TpFilmothequeApplication.class, args);
     }
 
 }
