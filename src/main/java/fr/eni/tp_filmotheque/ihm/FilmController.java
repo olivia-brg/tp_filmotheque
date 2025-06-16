2e8da31c7886b2e653cef4d1b196e0a1b79fe2d2
Olivia BERGAGLIA <olivia.bergaglia2025@campus-eni.fr>
2025-06-16 17:14:52 +0200

    thymeleaf layout courses

--- src/main/java/fr/eni/tp_filmotheque/ihm/FilmController.java
+++ src/main/java/fr/eni/tp_filmotheque/ihm/FilmController.java
@@ -3,6 +3,11 @@ package fr.eni.tp_filmotheque.ihm;
 import fr.eni.tp_filmotheque.bll.FilmService;
 import fr.eni.tp_filmotheque.bo.Film;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,21 +19,30 @@ public class FilmController {
         this.filmService = filmService;
     }
 
-    public void afficherFilms() {
-        System.out.println("\nTous les films : ");
+    @GetMapping({ "/","/films" })
+    public String afficherFilms(Model model) {
         List<Film> films = filmService.consulterFilms();
-        films.forEach(System.out::println);
+        model.addAttribute("films", films);
+        return "index";
     }
 
-    public void afficherUnFilm(long id) {
+    @GetMapping("/films/detail")
+    public String afficherUnFilm(@RequestParam(name="id") long id, Model model) {
+        Film current = null;
         if (id > 0) {// L'identifiant en base commencera en 1
-            Film current = filmService.consulterFilmParId(id);
-            if (current != null)
+            current = filmService.consulterFilmParId(id);
+            if (current != null) {
                 System.out.println(current);
-            else
+                model.addAttribute("film", current);
+                return "movie-detail";
+            }
+            else{
                 System.out.println("Film inconnu!!");
+                return "film-not-found";
+            }
         } else {
             System.out.println("Identifiant inconnu");
+            return "film-not-found";
         }
     }
 
